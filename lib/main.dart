7b90f100c52af8cbd9e0d66b99163d52b7db1e1b
henry-hue <80011640+henry-hue@users.noreply.github.com>
2024-06-29 16:56:04 -0600

    display clicked on plants info

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 
 import 'plant.dart';
 
+import 'package:collection/collection.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,9 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       title: 'PlantSelect',
       theme: ThemeData(
@@ -62,22 +61,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   List<Plant> favorites = [];
 
   Future<List<Plant>> fetchPlants() async {
-    final String url = 'https://script.googleusercontent.com/macros/echo?user_content_key=_B-W-AHmjR26KU5dTCw1S-B2DHZEuws01wTIWfteAhh1hJmlRPaKDGo9Y28yztqfS4hpvU0auyjWeXE6R04QW4DiUHEKgbgXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnO2U0Bl7BUAklHHeNRDrUcIoEcGPmrrlK_ulnafppH3w7o8FAM3ee_EkorPOGtTMgbRERG-Fn53JVefYCVkuXGQB2G7xa3afN9z9Jw9Md8uu&lib=MxnqXoKCpdNq7DADJrJEvDBtmPjijWW5o';
+    final String url =
+        'https://script.googleusercontent.com/macros/echo?user_content_key=_B-W-AHmjR26KU5dTCw1S-B2DHZEuws01wTIWfteAhh1hJmlRPaKDGo9Y28yztqfS4hpvU0auyjWeXE6R04QW4DiUHEKgbgXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnO2U0Bl7BUAklHHeNRDrUcIoEcGPmrrlK_ulnafppH3w7o8FAM3ee_EkorPOGtTMgbRERG-Fn53JVefYCVkuXGQB2G7xa3afN9z9Jw9Md8uu&lib=MxnqXoKCpdNq7DADJrJEvDBtmPjijWW5o';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
       List<dynamic> values = data.sublist(1);
       List<Plant> plants = values.map((json) => Plant.fromJson(json)).toList();
-    return plants;
+      return plants;
     } else {
       throw Exception('Failed to load plants');
     }
-}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,9 +86,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    
+
     return Scaffold(
-      
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
@@ -99,139 +97,138 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Image.asset('assets/images/logo.png'),
       ),
-        body: FutureBuilder<List<Plant>>(
-          future: fetchPlants(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  Plant plant = snapshot.data![index];
-                  return ListTile(
-                    title: Text(plant.values[0]),
-                    //leading: Image.network(plant.imageUrl),
-                    subtitle: Text('Botanic Name: ${plant.values[1]}'),
-                    trailing: Column(
-                      
-                      children: <Widget>[
-                    
-                        ElevatedButton(child: Text('Add Plant'), onPressed: () {favorites.add(plant);})
-                      ],
-                    ),
+      body: FutureBuilder<List<Plant>>(
+        future: fetchPlants(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                Plant plant = snapshot.data![index];
+                return ListTile(
+                  title: Text(plant.values[0]),
+                  //leading: Image.network(plant.imageUrl),
+                  subtitle: Text('Botanic Name: ${plant.values[1]}'),
+                  trailing: Column(
+                    children: <Widget>[
+                      ElevatedButton(
+                          child: Text('Add Plant'),
+                          onPressed: () {
+                            favorites.add(plant);
+                          })
+                    ],
+                  ),
 
-
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => selectedPlants(plant: plant),
-                          ),
-                      );
-                    },
-                  );
-                    
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Center(child: Text('${snapshot.error}'));
-            }
-            // By default, show a loading spinner
-            return Center(child: CircularProgressIndicator());
-          },
-        
-          
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Image.asset('assets/images/plantImage.png'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondScreen()),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => selectedPlants(plant: plant),
+                      ),
+                    );
+                  },
+                );
+              },
             );
-          },
-        ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        // bottomNavigationBar: BottomNavigationBar(items: [
+          } else if (snapshot.hasError) {
+            return Center(child: Text('${snapshot.error}'));
+          }
+          // By default, show a loading spinner
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Image.asset('assets/images/plantImage.png'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondScreen()),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // bottomNavigationBar: BottomNavigationBar(items: [
 
-        // ],)
-      );
-  }}
-
-  class SecondScreen extends StatelessWidget {
-    //final Plant favorites;
-
-    //SecondScreen({required this.favorites});
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-          title: Image.asset('assets/images/logo.png'),
-      //     title: Text('Your Plants', style: TextStyle(
-      // fontWeight: FontWeight.w300, // light
-      // fontStyle: FontStyle.italic, // italic ),
-      //   ),),),
-        ),
-          // body: Center(
-          //   child: Plant favorites,
-          // ),
-      );
-    }
+      // ],)
+    );
   }
+}
 
-  // ...
+class SecondScreen extends StatelessWidget {
+  //final Plant favorites;
+
+  //SecondScreen({required this.favorites});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Image.asset('assets/images/logo.png'),
+        //     title: Text('Your Plants', style: TextStyle(
+        // fontWeight: FontWeight.w300, // light
+        // fontStyle: FontStyle.italic, // italic ),
+        //   ),),),
+      ),
+      // body: Center(
+      //   child: Plant favorites,
+      // ),
+    );
+  }
+}
+
+// ...
 
 class selectedPlants extends StatelessWidget {
   final Plant plant;
+  final List<String> plantStr = [];
 
+  final List<String> attr = [
+    'Plant Type',
+    'Height',
+    'Width',
+    'Flowering Season',
+    'Flower Color',
+    'Sun',
+    'Water Needs',
+    'USDA Hardiness Zone',
+    'Soil Type',
+    'Deer Resistant',
+    'Good for Pollination',
+    'Winter Interest',
+    'North American Native',
+    'Year Introduced',
+    'Annual Commercial Maintenance',
+    '5-10 Year Commercial Maintenance',
+    'Elevation Guide	Description	Web Link'
+  ];
+  final List<String> plantInfo = [];
 
   selectedPlants({required this.plant});
   @override
   Widget build(BuildContext context) {
-    ListView.builder(
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(8),
-            itemCount: plant.values.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 50,
-                margin: EdgeInsets.all(2),
-                // color: msgCount[index]>=10? Colors.blue[400] msgCount[index]>3? Colors.blue[100]: Colors.grey,
-                child: Container(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              print("Do something!");
-                            },
-                            style: ButtonStyle(
-                                tapTargetSize:
-                                    MaterialTapTargetSize.shrinkWrap,
-                                shape: WidgetStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(40.0)),
-                                ),
-                                backgroundColor: WidgetStateProperty.all(
-                                    Colors.white)),
-                            child: Text(
-                              '${plant.values[index]} ',
-                              style: TextStyle(fontSize: 18),
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                        ]),
-                  ),
-                ),
-              );
-            });
-    
-    throw Exception('Failed to load plant info');
+    for (final value in plant.values) {
+      plantStr.add(value.toString());
+    }
+
+    for (final pairs in IterableZip([attr, plantStr])) {
+      plantInfo.add('${pairs[0]} : ${pairs[1]}');
+    }
+    return Table(
+        border: TableBorder.all(width: 1.0, color: Colors.black),
+        children: [
+          for (final info in plantInfo)
+            TableRow(children: [
+              TableCell(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  new Text(info),
+                ],
+              ))
+            ])
+        ]);
+
+    // throw Exception('Failed to load plant info');
   }
 }
-
