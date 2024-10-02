60c73d2ae50dffb06ea3b83ec69f2102cd6d1bdd
Rob Raymond <rraymond98@yahoo.com>
2024-10-01 21:24:23 -0600

    Move credentials to a different file.

--- lib/myplants.dart
+++ lib/myplants.dart
@@ -4,24 +4,7 @@ import 'plant.dart';
 import 'package:collection/collection.dart';
 import 'editplant.dart';
 import 'package:gsheets/gsheets.dart';
-
-const _credentials = r'''
-{
-  "type": "service_account",
-  "project_id": "gsheet-access-429203",
-  "private_key_id": "b3f0d46463047aa0dcc9c509f450b07d5b7ed5c6",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDLGxGD21I+u0I7\nPQ0gkjgVm+pMMQzSVUf2b0f1liJLz4jyIixsK5g42sxRheY/aSCLpxdzHHK94eOX\nrh9Lt72yaUyzf0JXgf0oGQQxWCQF1Q5jV1ERevd5j1+AShK8vDmNliKjrg7jTrsg\nK0+WzQP/B7PEeghH5vYmKEWO6eraoPYxw586845LvBHvv6VuaZw9HlOyOi7h1YnY\nJX8tFjcFUUGCrw4O5gTjSptUpXjba/TE8KQhcmy6WnHK5P8XVhUZ+arvME4Jr4pC\ncLyIxnd6V4aGWX82v8BMKvke4w06wEooMeaACJXRjPR1RKjnP8i9mYQyKTcWcp/3\n/oxcUfk3AgMBAAECggEAArj5qqFRgGrFMAMY/nNHUi7VXLEZoWYoQUrrlYSuMAL3\nrHs9yjNpZnW0YHFhjT8NAyI5w77BYP86rElWvQ6Y+d/EefF+Q6sH6DYxorGyReu9\ntiIxAYF6+MOtvYwApUwGn3nAPB50r6JaCsvZ1MUpxQzm4X7bRo93hDps+EX1cnnI\nwKpY2Lrvs5MT2ElZXJon1e4I63ipnsYy0JCtXxaCdLpZg+n9FCgr/AG4UoWE2g8B\nzrV341CaXMHxThfyPPF2bpDD/XqaOm5PPXOKDsKIxY1W6AmKLghwIRs2aQkE2IFg\n5VSRYEYl4KsSsHagaUMw5SB8bXa43bfqBWbGLC2CBQKBgQD3yC9M4DKULp12vScY\n4qFtrp2aEX+QzUsTkInIEw9S2P/e+3ti2ZQBlkfyGBp7MidQyV18cqXMXCcUiVDk\nfvJAHBIhxmNN6XFgR9D8zvLxweNANEdk/zbpUuKmUUOD5QsrpSeVFt9uMNHaAy/5\n2Y4NnXHrXMU/0gTDqAqcvQVcRQKBgQDR145YKPyW4wxqt9uHdevlrih5FGSgSMFd\nuYzfPQfAsrZui20T7R/Zk+h7/4onT0POLHNdEsEQdEI1LWX3E3NQGkJK33w4gNU1\nRI9IKY24sP2vQzmGTqq9wRZXr1ZtpOePqYQ2EJD/LQ6h8FAqyZ8I++jIfjduLZ4q\nBpQKvK29SwKBgQCiUiUScokP5B3JrI6RUd440TxzsuTjwmldbsGkLUBLoNa2h/7D\nug+onn+RTFMEw81XsiKpJR4Sa4g6ft5cYgFnGDyUnbduUfCxBzsUcqpa0A6Ef/sJ\nYdviLCdIl1HodMLFm9L7a1mBgT/oV1A1mFzj9HGtoz+g2H/AgTQ6i+WYFQKBgEqU\nDacPihYmQ7d5+K/AHULYDtJZiRneQbsJwyNkEWlPGr9XFkFKuVMe2jWXsIYgCb0w\n1x9xFuS+LPmxVNfnNch2TLFHlGKQhzYTU8kV26SUYtTzU3KEavJduY8YZeM6BIJu\ngOqmIKJQZS7wtc0/MyKCRU1cbl7eH6RYCi12uVjZAoGBAKb7zcAmxPDqnRonETOw\njfREMc5A55jL7Qr4PZ/Wx6cnSgEybCWZH1edRbwM/2aErmHDrtaoXzwC5Wf/mZbS\n2yz0Cv8aJkeVX7TBd9aGLkpGThkbcSxEQyqe6t2HWU1pimPOKhjDIbTLHPXSYey4\nTFpyCowSLY7YSHXlVEew1+85\n-----END PRIVATE KEY-----\n",
-  "client_email": "gsheets@gsheet-access-429203.iam.gserviceaccount.com",
-  "client_id": "116158027720527848898",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gsheets%40gsheet-access-429203.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
-''';
-
-const _spreadsheetId = '1lHYYVl_AJE5lBt4F43CtAvuO7G2xXFgRFZ0jc4fyrCc';
+import 'credentials.dart';
 
 class MyPlants extends StatefulWidget {
   const MyPlants(
@@ -39,9 +22,9 @@ class MyPlants extends StatefulWidget {
 class _MyPlantsState extends State<MyPlants> {
 
   sheetsPlants() async {
-    final gsheets = GSheets(_credentials);
+    final gsheets = GSheets(credentials);
     // fetch spreadsheet by its id
-    final ss = await gsheets.spreadsheet(_spreadsheetId);
+    final ss = await gsheets.spreadsheet(spreadsheetId);
 
     var sheet = ss.worksheetByTitle(widget.username);
       sheet ??= await ss.addWorksheet(widget.username);
