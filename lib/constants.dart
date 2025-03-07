2f2d1b124cff7b3b98f0d653d15410b0e6d79dab
jwilch <jwilch@wilch.net>
2025-03-07 12:05:03 -0700

    Support for SSL Cert

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const String apiUrl = 'http://35.90.142.227:3000';
+  static const String apiUrl = 'https://db.plantselect.org';
   static const String apiAuthToken = '8b29dcda6b6c77025493b34fe95437a451969c6d0887a4f0c3925c031c430d3d';
 }
